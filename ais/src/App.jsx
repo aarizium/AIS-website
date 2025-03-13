6fb0ccdb19b3e49fb2b1dbbdf8a951ede2d4c5a5
aarizium <aarizium@gmail.com>
2025-03-13 12:59:25 +0530

    changes

--- ais/src/App.jsx
+++ ais/src/App.jsx
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import HomeProduct from "./components/HomeProduct";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
